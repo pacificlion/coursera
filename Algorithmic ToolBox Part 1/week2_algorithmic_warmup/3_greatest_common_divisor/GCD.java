e198f7144b27847299cc5cb5567dd32c74321eb6
Prashant Singh <singhpra@usc.edu>
2023-04-30 19:49:33 -0700

    added solutions for week 2- gcd, lcm

--- Algorithmic ToolBox Part 1/week2_algorithmic_warmup/3_greatest_common_divisor/GCD.java
+++ Algorithmic ToolBox Part 1/week2_algorithmic_warmup/3_greatest_common_divisor/GCD.java
@@ -3,7 +3,7 @@ import java.util.*;
 public class GCD {
   private static int gcd_naive(int a, int b) {
     int current_gcd = 1;
-    for(int d = 2; d <= a && d <= b; ++d) {
+    for (int d = 2; d <= a && d <= b; ++d) {
       if (a % d == 0 && b % d == 0) {
         if (d > current_gcd) {
           current_gcd = d;
@@ -14,11 +14,31 @@ public class GCD {
     return current_gcd;
   }
 
+  private static int gcd(int a, int b) {
+
+    if (a == 0) {
+      return b;
+    }
+    if (b == 0) {
+      return a;
+    }
+    if(a==1||b==1){
+      return 1;
+    }
+    if (a == b) {
+      return a;
+    } else if (a > b) {
+      return gcd(a %b, b);
+    } else {
+      return gcd(a, b% a);
+    }
+  }
+
   public static void main(String args[]) {
     Scanner scanner = new Scanner(System.in);
     int a = scanner.nextInt();
     int b = scanner.nextInt();
 
-    System.out.println(gcd_naive(a, b));
+    System.out.println(gcd(a, b));
   }
 }
--- Algorithmic ToolBox Part 1/week2_algorithmic_warmup/4_least_common_multiple/LCM.java
+++ Algorithmic ToolBox Part 1/week2_algorithmic_warmup/4_least_common_multiple/LCM.java
@@ -9,11 +9,36 @@ public class LCM {
     return (long) a * b;
   }
 
+  private static int gcd(int a, int b) {
+
+    if (a == 0) {
+      return b;
+    }
+    if (b == 0) {
+      return a;
+    }
+    if(a==1||b==1){
+      return 1;
+    }
+    if (a == b) {
+      return a;
+    } else if (a > b) {
+      return gcd(a %b, b);
+    } else {
+      return gcd(a, b% a);
+    }
+  }
+
+  private static long lcm(int a, int b) {
+    long product =(long)a *(long)b;
+    return product / (long)gcd(a, b);
+  }
+
   public static void main(String args[]) {
     Scanner scanner = new Scanner(System.in);
     int a = scanner.nextInt();
     int b = scanner.nextInt();
 
-    System.out.println(lcm_naive(a, b));
+    System.out.println(lcm(a, b));
   }
 }
